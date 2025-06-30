e92ea58d7251b60a7bdfacbf185b36358e5d80c5
Vivek Kanjariya <144126963+vivek-kanjariya@users.noreply.github.com>
2025-06-30 19:11:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-green-600 text-white p-6 rounded-xl text-xl text-center">
+      Page is Under Counstruction
+      Your Application has Successfully Deployed :0
+    </div>
+  );
+}
+
+export default App;
