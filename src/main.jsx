683c3e63d02f1e5a5933ab71316fcde19776051f
Vivek Kanjariya <144126963+vivek-kanjariya@users.noreply.github.com>
2025-07-13 17:34:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css"; // ← THIS LINE
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode> 
-// );
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -17,8 +6,10 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <div id="zoom-wrapper">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </div>
   </React.StrictMode>
 );
